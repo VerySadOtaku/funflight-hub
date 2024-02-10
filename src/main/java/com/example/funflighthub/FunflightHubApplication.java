a6467d161359c663263a8ce5a52587debe175d0c
VerySadOtaku <german.sichenikov@gmail.com>
2024-02-10 13:20:55 +0200

    first commit

---
+++ src/main/java/com/example/funflighthub/FunflightHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.funflighthub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunflightHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FunflightHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/funflighthub/FunflightHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.funflighthub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunflightHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
